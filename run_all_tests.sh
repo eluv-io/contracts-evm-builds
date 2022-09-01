837214ccbc6c1b497f75957774aaddd2ad4bbda3
elv-gilles <gilles.gaillard@eluv.io>
2022-09-01 19:06:36 +0200

    mod run_all_tests.sh to run top level tests

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+pushd contracts-go > /dev/null
+go test -count=1 .
+popd > /dev/null
+
 for dir in contracts-go/v*; do
     pushd $dir > /dev/null
-    go test ./...
+    go test -count=1 ./...
     popd > /dev/null
 done
 echo "done"
