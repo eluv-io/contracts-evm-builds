aea12586f120db62985a9539e01f79bec92d7f99
elv-preethi <39742400+elv-preethi@users.noreply.github.com>
2023-02-22 08:27:42 -0800

    update solc script

--- scripts/solc.sh
+++ scripts/solc.sh
@@ -13,70 +13,34 @@ function run_solc() {
     solc_bin="$(solc_dir)/${version}/solc"
     local bsc_name
     local out=$3
-    local ext_lib
-
-    local ext_lib_enabled
+    local files="$2"
     if [ $# -gt 3 ]; then
-      ext_lib_enabled=true
-      ext_lib=$4
+        files="$4 ${files}"
     fi
 
     bsc_name=$(basename "${2}")
 
     if is_debug; then
-       if [ ${ext_lib_enabled} ]; then
-         echo "${solc_bin} ${ext_lib} ${2} --abi --hashes --optimize -o ${out}/abi --overwrite"
-       else
-           echo "${solc_bin} ${2} --abi --hashes --optimize -o ${out}/abi --overwrite"
-       fi
+        echo "${solc_bin} ${files} --abi --hashes --optimize -o ${out}/abi --overwrite"
     fi
 
-    if [ ${ext_lib_enabled} ]; then
-        if ! ${solc_bin} "${ext_lib}" "${2}" --abi --hashes --optimize -o "${out}/abi" --overwrite; then
-              fail "failed generating ABI and function hashes of ${bsc_name} to ${out}/abi"
-        fi
-    else
-        if ! ${solc_bin} "${2}" --abi --hashes --optimize -o "${out}/abi" --overwrite; then
-              fail "failed generating ABI and function hashes of ${bsc_name} to ${out}/abi"
-        fi
+    if ! ${solc_bin} ${files} --abi --hashes --optimize -o "${out}/abi" --overwrite; then
+        fail "failed generating ABI and function hashes of ${bsc_name} to ${out}/abi"
     fi
-
 
     if is_debug; then
-      if [ ${ext_lib_enabled} ]; then
-        echo "${solc_bin} ${ext_lib} ${2} --bin --optimize -o ${out}/bin --overwrite"
-      else
-        echo "${solc_bin} ${2} --bin --optimize -o ${out}/bin --overwrite"
-      fi
+      echo "${solc_bin} ${files} --bin --optimize -o ${out}/bin --overwrite"
     fi
 
-    if [ ${ext_lib_enabled} ]; then
-        if ! ${solc_bin} "${ext_lib}" "${2}" --bin --optimize -o "${out}/bin" --overwrite; then
-          fail "failed generating BIN of ${bsc_name} to ${out}/bin"
-        fi
-    else
-        if ! ${solc_bin} "${2}" --bin --optimize -o "${out}/bin" --overwrite; then
-          fail "failed generating BIN of ${bsc_name} to ${out}/bin"
-        fi
+    if ! ${solc_bin} ${files} --bin --optimize -o "${out}/bin" --overwrite; then
+       fail "failed generating BIN of ${bsc_name} to ${out}/bin"
     fi
-
 
     if is_debug; then
-      if [ ${ext_lib_enabled} ]; then
-            echo "${solc_bin} ${ext_lib} ${2} --combined-json bin,bin-runtime,srcmap,srcmap-runtime,abi,userdoc,devdoc,metadata,hashes --optimize -o ${out}/combined-json --overwrite"
-      else
-            echo "${solc_bin} ${2} --combined-json bin,bin-runtime,srcmap,srcmap-runtime,abi,userdoc,devdoc,metadata,hashes --optimize -o ${out}/combined-json --overwrite"
-      fi
+     echo "${solc_bin} ${files} --combined-json bin,bin-runtime,srcmap,srcmap-runtime,abi,userdoc,devdoc,metadata,hashes --optimize -o ${out}/combined-json --overwrite"
     fi
 
-    if [ ${ext_lib_enabled} ]; then
-          if ! ${solc_bin} ${ext_lib} "${2}" --combined-json bin,bin-runtime,srcmap,srcmap-runtime,abi,userdoc,devdoc,metadata,hashes --optimize -o "${out}/combined-json" --overwrite; then
-                fail "failed producing combined json for $bsc_name"
-          fi
-    else
-          if ! ${solc_bin} "${2}" --combined-json bin,bin-runtime,srcmap,srcmap-runtime,abi,userdoc,devdoc,metadata,hashes --optimize -o "${out}/combined-json" --overwrite; then
-                fail "failed producing combined json for $bsc_name"
-          fi
+    if ! ${solc_bin} ${files} --combined-json bin,bin-runtime,srcmap,srcmap-runtime,abi,userdoc,devdoc,metadata,hashes --optimize -o "${out}/combined-json" --overwrite; then
+     fail "failed producing combined json for $bsc_name"
     fi
-
 }
